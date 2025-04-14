4333d610265aeb3a296d334ccc37e2c1be148f29
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-04-15 01:44:42 +0330

    Ability to add new contact

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import ContactsList from "./ContactsList";
 
 function Contacts() {
+  const [contacts, setContacts] = useState([]);
   const [contact, setContact] = useState({
     name: "",
     lastName: "",
@@ -16,7 +18,14 @@ function Contacts() {
   };
 
   const addHandler = () => {
-    console.log(contact);
+    setContacts((contacts) => [...contacts, contact])
+    setContact({
+        name: "",
+        lastName: "",
+        email: "",
+        phone: "",
+
+    })
   };
 
   return (
@@ -52,6 +61,7 @@ function Contacts() {
         />
         <button onClick={addHandler}>Add contact</button>
       </div>
+      <ContactsList contacts={contacts} />
     </div>
   );
 }
---
+++ src/components/ContactsList.jsx
@@ -0,0 +1,8 @@
+function ContactsList(props) {
+    console.log(props);
+  return (
+    <div>ContactsList</div>
+  )
+}
+
+export default ContactsList
