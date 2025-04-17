5c88d597f41adc93814a8c78cab2a2812f7dbe9b
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-04-17 18:54:30 +0330

    adding css to components

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,11 +1,12 @@
 import ContactItem from "./ContactItem";
+import styles from "./contactList.module.css";
 
 function ContactsList({ contacts, deleteHandler }) {
   return (
-    <div>
+    <div className={styles.container}>
       <h3>Contacts List</h3>
       {contacts.length ? (
-        <ul>
+        <ul className={styles.contacts}>
           {contacts.map((contact) => (
             <ContactItem
               key={contact.id}
@@ -15,7 +16,7 @@ function ContactsList({ contacts, deleteHandler }) {
           ))}
         </ul>
       ) : (
-        <p>No Contact Yet!</p>
+        <p className={styles.message}>No Contact Yet!</p>
       )}
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
 
 function Header() {
   return (
     <div className={styles.container}>
-        <h1>Contact App</h1>
+      <h1>Contact App</h1>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
